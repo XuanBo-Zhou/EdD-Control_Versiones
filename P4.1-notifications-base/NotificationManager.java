1f70adac39908bf5a33d33af3893a7f623d838a1
XuanBo-Zhou <xuanb.zho@iesmariamoliner.com>
2026-05-31 01:39:40 +0200

    refactor: Creado clase pushService que implementa NotificationService

--- P4.1-notifications-base/NotificationManager.java
+++ P4.1-notifications-base/NotificationManager.java
@@ -17,17 +17,20 @@ class SMSService implements NotificationService{
     }
 }
 
+class PushService implements NotificationService{
+    @Override
+    public void send(String message, String recipient) {
+        // Código para notificación push
+        System.out.println("Enviando push a " + recipient + ": " + message);
+        // Lógica compleja de push aquí...
+    }
+}
+
 public class NotificationManager {
-    // TODO: Separar en clases diferentes: EmailService, SMSService, PushService
     // TODO: Aplicar patrón Strategy para los tipos de notificación
     // TODO: Añadir sistema de logs
     
     public void send(String type, String message, String recipient) {
-        if (type.equals("push")) {
-            // Código para notificación push
-            System.out.println("Enviando push a " + recipient + ": " + message);
-            // Lógica compleja de push aquí...
-        }
     }
     
     // TODO: Añadir método para enviar a múltiples destinatarios
