e509cfaaadcb06bcd99dcc357f9b74505bea27c4
XuanBo-Zhou <xuanb.zho@iesmariamoliner.com>
2026-05-31 18:30:38 +0200

    refactor: Se ha refactorizado el Main para implementar un Try-Catch

--- P4.1-notifications-base/Main.java
+++ P4.1-notifications-base/Main.java
@@ -5,8 +5,22 @@ public class Main {
         NotificationManager manager = new NotificationManager();
         
         // Probamos todas las funcionalidades
-        manager.send("email", "Bienvenido al sistema", "usuario@email.com");
-        manager.send("sms", "Tu código es 1234", "+34123456789");
-        manager.send("push", "Tienes un nuevo mensaje", "user_device_001");
+        try {
+            manager.send("email", "Bienvenido al sistema", "usuario@email.com");
+        } catch (InvalidNotificationException e) {
+            System.out.println("ERROR: " + e);
+        }
+
+        try {
+            manager.send("sms", "Tu código es 1234", "+34123456789");
+        } catch (InvalidNotificationException e) {
+            System.out.println("ERROR: " + e);
+        }
+
+        try {
+            manager.send("push", "Tienes un nuevo mensaje", "user_device_001");
+        } catch (InvalidNotificationException e) {
+            System.out.println("ERROR: " + e);
+        }
     }
 }
