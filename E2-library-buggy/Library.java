49d050dca5dc3d98b3f4f5128ae24f2f3081eb0f
XuanBo-Zhou <xuanb.zho@iesmariamoliner.com>
2026-06-07 18:37:37 +0200

    fix: filtrar libros según disponibilidad real
    - Usar isAvailable() para comprobar disponibilidad
    - Añadir tests unitarios
    
    Closes #7

--- E2-library-buggy/Library.java
+++ E2-library-buggy/Library.java
@@ -25,14 +25,14 @@ public class Library {
     
     public List<Book> findAvailableBooks() {
         List<Book> availableBooks = new ArrayList<>();
-        // BUG 6: ConcurrentModificationException potencial
         for (Book book : books) {
-            if (true) { // BUG 7: Siempre true, no verifica disponibilidad real
+            if (book.isAvailable()){
                 availableBooks.add(book);
             }
         }
         return availableBooks;
     }
+
     
     // BUG 8: Falta método para quitar libros
 }
